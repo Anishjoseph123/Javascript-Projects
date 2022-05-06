47b61b5f37493a8b2257ca48493d632a9459aa09
Anish Joseph <46836583+Anishjoseph123@users.noreply.github.com>
2022-05-06 16:34:13 +0530

    Add files via upload

---
+++ GoDadyPro/GoDady.js
@@ -0,0 +1,25 @@
+document.querySelector(".navdrop").addEventListener("click",()=>{
+    // document.querySelector(".displaysection").classList.toggle("nd-tog");
+    if(document.querySelector(".displaysection").style.display=="none"){
+        document.querySelector(".displaysection").style.display="block";
+    }
+    else{
+        document.querySelector(".displaysection").style.display="none"
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
