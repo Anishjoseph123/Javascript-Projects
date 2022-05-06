fb557dd9d1675827c5e6805370fc075c1e59eb8f
Anish Joseph <46836583+Anishjoseph123@users.noreply.github.com>
2022-05-06 16:44:05 +0530

    Add files via upload
    Rainbow using Java

---
+++ js/rainbow.js
@@ -0,0 +1,84 @@
+let violet = document.getElementById("v");
+violet.style.color = "violet";
+violet.style.color = "violet";
+violet.style.border = "1px dotted violet";
+violet.style.height = "30px";
+violet.addEventListener("mouseover", () => {
+  violet.style.backgroundColor = "violet";
+});
+violet.addEventListener("mouseleave", () => {
+  violet.style.backgroundColor = "white";
+});
+let indigo = document.getElementById("i");
+indigo.style.color = "lightblue";
+indigo.style.border = "1px solid lightblue";
+indigo.style.height = "25px";
+indigo.style.marginTop = "10px";
+indigo.addEventListener("mouseover", () => {
+  indigo.style.backgroundColor = "lightblue";
+});
+indigo.addEventListener("mouseleave", () => {
+  indigo.style.backgroundColor = "white";
+});
+let blue = document.getElementById("b");
+blue.style.color = "blue";
+blue.style.border = "1px solid blue";
+blue.style.height = "25px";
+blue.style.marginTop = "10px";
+blue.addEventListener("mouseover", () => {
+  blue.style.backgroundColor = "blue";
+});
+blue.addEventListener("mouseleave", () => {
+  blue.style.backgroundColor = "white";
+});
+let green = document.getElementById("g");
+green.style.color = "green";
+green.style.border = "1px solid green";
+green.style.height = "25px";
+green.style.marginTop = "10px";
+green.addEventListener("mouseover", () => {
+  green.style.backgroundColor = "green";
+});
+green.addEventListener("mouseleave", () => {
+  green.style.backgroundColor = "white";
+});
+let yellow = document.getElementById("y");
+yellow.style.color = "yellow";
+yellow.style.border = "1px solid yellow";
+yellow.style.height = "25px";
+yellow.style.marginTop = "10px";
+yellow.addEventListener("mouseover", () => {
+  yellow.style.backgroundColor = "yellow";
+});
+yellow.addEventListener("mouseleave", () => {
+  yellow.style.backgroundColor = "white";
+});
+let orange = document.getElementById("o");
+orange.style.color = "orange";
+orange.style.border = "1px solid orange";
+orange.style.height = "25px";
+orange.style.marginTop = "10px";
+orange.addEventListener("mouseover", () => {
+  orange.style.backgroundColor = "orange";
+});
+orange.addEventListener("mouseleave", () => {
+  orange.style.backgroundColor = "white";
+});
+let red = document.getElementById("r");
+red.style.color = "red";
+red.style.border = "1px solid red";
+red.style.height = "25px";
+red.style.marginTop = "10px";
+red.addEventListener("mouseover", () => {
+  red.style.backgroundColor = "red";
+});
+red.addEventListener("mouseleave", () => {
+  red.style.backgroundColor = "white";
+});
+let b = document.getElementById("bo");
+bo.style.backgroundImage =
+  "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx-y5AUR7qQuGsRwyX01KUrZiiGOfEEl29WA&usqp=CAU')";
+bo.style.height = "300px";
+bo.style.backgroundRepeat = "no-repeat";
+bo.style.backgroundSize = "cover"
+bo.style.backgroundPosition = "center";
